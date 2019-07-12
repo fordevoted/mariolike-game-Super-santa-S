1c53b7417c1203d5cf593113cedc9db5de53cb3f
Yu-Wei, Chen <210509fssh@gmail.com>
2019-07-12 14:28:57 +0800

    upload make file

---
+++ make.bat
@@ -0,0 +1,47 @@
+﻿@echo off
+REM make
+REM Assembles and links the 32-bit ASM program into .exe which can be used by WinDBG
+REM Uses MicroSoft Macro Assembler version 6.11 and 32-bit Incremental Linker version 5.10.7303
+REM Created by Huang 
+
+REM delete related files
+del mario.lst	exc
+del mario.obj
+del mario.ilk
+del mario.pdb
+del mario.exe
+
+setlocal 
+set INCLUDE=C:\WINdbgFolder\;	
+set LIB=C:\WINdbgFolder\;
+set PATH=C:\WINdbgFolder\;
+
+REM /c          assemble without linking
+REM /coff       generate object code to be linked into flat memory model 
+REM /Zi         generate symbolic debugging information for WinDBG
+REM /Fl		Generate a listing file
+ 
+
+ML /c /coff /Zi   mario.asm
+if errorlevel 1 goto terminate
+
+REM /debug              generate symbolic debugging information
+REM /subsystem:console  generate console application code
+REM /entry:start        entry point from WinDBG to the program 
+REM                           the entry point of the program must be _start
+
+REM /out:%1.exe         output %1.exe code
+REM %1.obj              input %1.obj
+REM Kernel32.lib        library procedures to be invoked from the program
+REM irvine32.lib
+REM user32.lib
+
+LINK /INCREMENTAL:no /debug /subsystem:console /entry:start /out:mario.exe mario.obj Kernel32.lib irvine32.lib user32.lib
+if errorlevel 1 goto terminate
+
+REM Display all files related to this program:
+DIR mario.*
+
+:terminate
+pause
+endlocal
